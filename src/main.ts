5a88d3d82ff058d19b482ecd205a7af2fe4fe620
jeffersonLucass <jeffersonlucas810@gmail.com>
2025-05-12 21:46:48 -0300

    interface swagger adicionado

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,8 +11,17 @@ async function bootstrap() {
       whitelist: true,
     }),
     );
-
-
+  const config = new DocumentBuilder()
+  .setTitle('NestJS API')
+  .setDescription('The NestJS API description')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(process.env.PORT ?? 3000);
+
 }
+
+
 bootstrap();
